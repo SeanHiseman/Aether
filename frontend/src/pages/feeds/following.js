462d10f4165197e1132c48b70665526c27aced6c
Sean Hiseman <seanhiseman@gmail.com>
2024-03-26 19:22:55 +0000

    Added following feed

--- frontend/src/pages/feeds/following.js
+++ frontend/src/pages/feeds/following.js
@@ -3,10 +3,34 @@ import React, { useEffect, useState } from 'react';
 import ContentWidget from '../content_widget';
 
 function FollowingPage() {
+    const [posts, setPosts] = useState([]);
+    //Gets posts from profiles and groups followed by user
+    useEffect(() => {
+        axios.get('/api/following_posts')
+        .then(response => {
+            setPosts(response.data);
+        })
+        .catch(error => {
+             console.error('Error getting posts:', error);
+        });
+    }, []);
+
     document.title = "Following";
     return (
         <div className="home-container">
             <div className="content-feed">
+                <div id="channel-content">
+                        {posts.length > 0 ? (
+                            <ul className="content-list">
+                                {posts.map(post => (
+                                    <ContentWidget key={post.post_id} isGroup={post.isGroup} post={post}/>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>No posts yet</p>
+                        )
+                    }
+                </div>
             </div>
             <aside id="right-aside">
                 <h1>Following</h1>
