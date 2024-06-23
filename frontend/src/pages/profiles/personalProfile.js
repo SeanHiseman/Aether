61051625f7068eccc406fe4f6226a6f6cea45a8f
Sean Hiseman <seanhiseman@gmail.com>
2024-06-23 19:43:32 +0100

    Created separate settings page

--- frontend/src/pages/profiles/personalProfile.js
+++ frontend/src/pages/profiles/personalProfile.js
@@ -11,11 +11,6 @@ const PersonalProfile = () => {
     const [newChannelName, setNewChannelName] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [friendRequests, setFriendRequests] = useState('');
-    const [isEditingBio, setIsEditingBio] = useState(false);
-    const [isEditingName, setIsEditingName] = useState(false);
-    const [isPhotoFormVisible, setIsPhotoFormVisible] = useState(false);
-    const [newBio, setBio] = useState('');
-    const [newName, setName] = useState('');
     const [profile, setProfile] = useState('');
     const [showChannelForm, setShowChannelForm] = useState(false);
     const [showFriendRequests, setShowFriendRequests] = useState(false);
@@ -51,20 +46,6 @@ const PersonalProfile = () => {
             setChannels([]);
         });
     }, [profile.profileId]);   
-    
-    //Set name in text area to current description
-    useEffect(() => {
-        if (isEditingName) {
-            setName(profile.username);
-        }
-    }, [isEditingName, profile.username]);
-
-    //Set bio in text area to current bio
-    useEffect(() => {
-        if (isEditingBio) {
-            setBio(profile.bio);
-        }
-    }, [isEditingBio, profile.bio]);
 
     //Adds channel to profile
     const AddChannel = async (event) => {
@@ -86,44 +67,8 @@ const PersonalProfile = () => {
             setErrorMessage(error.response ? error.response.data.error : 'Failed to add channel. Please try again.');
         }
     };  
-
-    const ChangeProfilePhoto = async (event) => {
-        try {
-            event.preventDefault();
-            const fileInput = event.target.elements.new_profile_photo;
-            if (!fileInput.files[0]) {
-                setErrorMessage('Please upload an image');
-                return;
-            }
-            const formData = new FormData();
-            formData.append('new_profile_photo', fileInput.files[0]);
-            const response = await axios.put(`/api/update_profile_photo/${profile.profileId}`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                }, 
-            })
-            setProfile(prevDetails => ({
-                ...prevDetails,
-                profilePhoto: response.data.newPhotoPath
-            }));
-            setIsPhotoFormVisible(false);
-        } catch(error) {
-            setErrorMessage('Error updating photo', error.response ? error.response.data : error);
-        };
-    }
     
     const channelRender = channels.find(c => c.channel_name === channel_name);
-
-    const deleteAccount = async () => {
-        try {
-            const response = await axios.delete('/api/delete_account', { data: { user_id: profile.userId } });
-                if (response.data.success) {
-                    navigate('/login');
-                }
-        } catch (error) {
-            setErrorMessage('Error deleting account:', error);
-        }
-    };
 
     const deleteChannel = async () => {
         try {
@@ -162,21 +107,6 @@ const PersonalProfile = () => {
             setErrorMessage("Error handling request:", error);
         }
     };
-    
-    const handleLogout = (event) => {
-        event.preventDefault();
-        axios.post('/api/logout')
-            .then(response => {
-                if (response.data.success) {
-                    navigate('/login');
-                } else {
-                    alert('Logout failed: ', response.data.message);
-                }
-            })
-            .catch(error => {
-                alert('Error during logout: ', error);
-            })
-    }
 
     //Uploads content 
     const handlePostSubmit = async (formData) => {
@@ -194,54 +124,10 @@ const PersonalProfile = () => {
         }
     };
 
-    //Changes profile bio
-    const handleUpdateBio = async () => {
-        try {
-            await axios.post('/api/change_bio', {
-                bio: newBio,
-                profileId: profile.profileId
-            });
-            setProfile({ ...profile, bio: newBio});
-            setIsEditingBio(false);
-        }
-        catch (error) {
-            setErrorMessage(`Error changing bio: ${error}`);
-        }
-    }; 
-
-    //Changes username
-    const handleUpdateName = async () => {
-        try {
-            await axios.post('/api/change_username', {
-                username: newName,
-                userId: profile.userId
-            });
-            setProfile({ ...profile, username: newName });
-            setIsEditingName(false);
-        }
-        catch (error) {
-            setErrorMessage(`Error changing name: ${error}`);
-        }
-    }; 
-
     //Toggles display of create channel form after button is pressed
     const toggleChannelForm = () => {
         setShowChannelForm(!showChannelForm)
     }
-
-    //Changes profile between public and private
-    const togglePrivate = async () => {
-        try {
-            const profile_id = profile.profileId;
-            const response = await axios.post('/api/toggle_private_profile', { profile_id} );
-            setProfile(prevDetails => ({
-                ...prevDetails,
-                isPrivate: response.data.is_private
-            }));
-        } catch (error) {
-            setErrorMessage('Error changing private status:', error);
-        }
-    };
 
     document.title = profile.username || "Profile";
     return (
@@ -249,78 +135,28 @@ const PersonalProfile = () => {
             <div className="content-feed">
                 <header id="profile-header">
                     <div id="profile-options">
+                        <Link to={`/settings/${username}`}>
+                            <button className="button">Settings</button>
+                        </Link>
                         <button className="button" onClick={getFriendRequests}>
                             {showFriendRequests ? 'Close requests' : 'See friend requests'}
                         </button>
-                        <button className="button" onClick={() => togglePrivate()}>{profile.isPrivate ? "Profile: private" : "Profile: public"}</button>
                         <p>{profile.followerCount} followers</p>   
                     </div>
-                    <form action="/api/logout" method="post" onSubmit={handleLogout}>
-                        <button className="button" type="submit">Logout</button>
-                    </form>
                     <div id="viewed-profile-info">
                         <div id="name-section">
-                            {isEditingName ? (
-                                <div className="change-name">
-                                    <button className='button' onClick={() => setIsEditingName(false)}>Close</button>
-                                    <textarea className="change-name-area" value={newName} onChange={(e) => {
-                                        const input = e.target.value;
-                                        const inputLength = input.length;
-                                        if (inputLength <= 100) {
-                                            setName(input)
-                                        } else {
-                                            setErrorMessage('Name cannot exceed 100 characters.');
-                                        }
-                                    }}
-                                    />
-                                    <button className="button" onClick={() => {setIsEditingName(false); handleUpdateName();}}>Save</button>
-                                </div>
-                            ) : (
-                                <div className="view-name">
-                                    <p className="large-text">{profile.username}</p>
-                                    <button className="button" onClick={() => setIsEditingName(true)}>Edit</button>
-                                </div>
-                            )}
+                            <div className="view-name">
+                                <p className="large-text">{profile.username}</p>
+                            </div>
                         </div>
                         <div id="bio-section">
-                            {isEditingBio ? (
-                                <div className="change-bio">
-                                    <button className='button' onClick={() => setIsEditingBio(false)}>Close</button>
-                                    <textarea className="change-text-area" value={newBio} onChange={(e) => {
-                                        const input = e.target.value;
-                                        const inputLength = input.length;
-                                        if (inputLength <= 1000) {
-                                            setBio(input)
-                                        } else {
-                                            setErrorMessage('Bio cannot exceed 1000 characters.');
-                                        }
-                                    }}
-                                    />
-                                    <button className="button" onClick={() => {setIsEditingBio(false); handleUpdateBio();}}>Save</button>
-                                </div>
-                            ) : (
-                                <div className="view-bio">
-                                    <p id="profile-bio">{profile.bio}</p>
-                                    <button className="button" onClick={() => setIsEditingBio(true)}>Edit bio</button>
-                                </div>
-                            )}
-                            {errorMessage && <div className="error-message">{errorMessage}</div>}
+                            <div className="view-bio">
+                                <p id="profile-bio">{profile.bio}</p>
+                            </div>
                         </div>
-                        <button className="button" onClick={deleteAccount}>Delete account</button>
                     </div>
                     <div id="profile-header-photo">
                         <img className="large-profile-photo" src={`/${profile.profilePhoto}`} alt="Profile" />
-                        <button className="button" onClick={() => setIsPhotoFormVisible(!isPhotoFormVisible)}>
-                            {isPhotoFormVisible ? 'Close' : 'Change Profile Photo'}
-                        </button>
-                        {isPhotoFormVisible && (
-                            <form id="change-profile-photo" action="/api/update_profile_photo" method="post" enctype="multipart/form-data" onSubmit={ChangeProfilePhoto}>
-                                <label htmlFor="new_profile_photo">Change Profile photo:</label>
-                                <input type="file" id="new_profile_photo" name="new_profile_photo" accept="image/*" />
-                                {errorMessage && <div className="error-message">{errorMessage}</div>}
-                                <input className="button" type="submit" value="Update" />
-                            </form>
-                        )}
                     </div>
                 </header>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
                 <div className="channel-feed">
