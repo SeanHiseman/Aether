6e03ce593cd5540eaca034d7515c8fb8e61168de
Sean Hiseman <seanhiseman@gmail.com>
2024-06-25 15:58:22 +0100

    Added group settings page

--- frontend/src/pages/groups/groupWrapper.js
+++ frontend/src/pages/groups/groupWrapper.js
@@ -3,13 +3,13 @@ import { useParams, useNavigate } from 'react-router-dom';
 import GroupHome from './groupHome';
 
 function GroupWrapper() {
-    const { group_name, channel_name } = useParams();
+    const { group_name, channel_name, channel_mode } = useParams();
     const navigate = useNavigate();
 
     //Directs to group main channel by default
     useEffect(() => {
-        if (!channel_name) {
-            navigate(`/group/${group_name}/Main`, { replace: true });
+        if (!channel_name || !channel_mode) {
+            navigate(`/group/${group_name}/Main/post`, { replace: true });
         }
     }, [group_name, channel_name, navigate]);
 
