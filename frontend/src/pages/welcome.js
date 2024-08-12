9e19c9e17876ee27baf58710136b701b412c5454
Sean Hiseman <seanhiseman@gmail.com>
2024-08-12 23:07:29 +0100

    Adjusted site entrance

--- frontend/src/pages/welcome.js
+++ frontend/src/pages/welcome.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import '../css/welcome.css';
 
 const Welcome = () => {
+    const [currentQuery, setCurrentQuery] = useState('');
     const [currentView, setCurrentView] = useState('welcome');
+    const navigate = useNavigate();
 
+    //Sends to ask page
+    const handleAskClick = (event) => {
+        event.preventDefault();
+        navigate('/ask', { state: { query: currentQuery } });
+    };
+    //Sends to search page
+    const handleSearchClick = (event) => {
+        event.preventDefault();
+        navigate(`/search?keyword=${currentQuery}`);
+    };
+    
     //Switches between settings states
     const renderComponent = () => {
         switch (currentView) {
@@ -13,7 +27,14 @@ const Welcome = () => {
                     <div className="channel-content">
                         <p class="text36">Welcome to Aether</p>
                         <p class="text24">Better social media</p>
-                        <button className="button join">Join for free</button>
+                        <div id="join-login">
+                            <Link to="/join">
+                                <button className="light-button join">Join</button>
+                            </Link>
+                            <Link to="/login">
+                                <button className="light-button join">Login</button>
+                            </Link>
+                        </div>
                     </div>
                 );
             case 'groups':
@@ -22,7 +43,9 @@ const Welcome = () => {
                         <p class="text36">Groups</p>
                         <p class="text24">Join public groups</p>
                         <p class="text24">Create groups with your friends</p>
-                        <button className="button join">Join for free</button>
+                        <Link to="/join">
+                            <button className="light-button join">Join</button>
+                        </Link>
                     </div>
                 );
             case 'profiles':
@@ -31,7 +54,9 @@ const Welcome = () => {
                         <p class="text36">Profiles</p>
                         <p class="text24">Different feeds for different content</p>
                         <p class="text24">Public or private</p>
-                        <button className="button join">Join for free</button>
+                        <Link to="/join">
+                            <button className="light-button join">Join</button>
+                        </Link>
                     </div>
                 );
             case 'posts':
@@ -39,7 +64,9 @@ const Welcome = () => {
                     <div className="channel-content">
                         <p class="text36">Posts</p>
                         <p class="text24">Images, videos and text all in one</p>
-                        <button className="button join">Join for free</button>
+                        <Link to="/join">
+                            <button className="light-button join">Join</button>
+                        </Link>
                     </div>
                 );
             case 'feeds':
@@ -47,7 +74,10 @@ const Welcome = () => {
                     <div className="channel-content">
                         <p class="text36">Feeds</p>
                         <p class="text24">View posts from friends, following and recommendations</p>
-                        <button className="button join">Join for free</button>
+                        <p class="text24">Create custom feeds</p>
+                        <Link to="/join">
+                            <button className="light-button join">Join</button>
+                        </Link>
                     </div>
                 );
             case 'algorithm':
@@ -55,8 +85,12 @@ const Welcome = () => {
                     <div className="channel-content">
                         <p class="text36">Algorithm</p>
                         <p class="text24">Focused on quality, not quantity</p>
+                        <p class="text24">Multiple votes on content</p>
+                        <p class="text24">Misinformation is controlled</p>
                         <p class="text24">You are in control</p>
-                        <button className="button join">Join for free</button>
+                        <Link to="/join">
+                            <button className="light-button join">Join</button>
+                        </Link>
                     </div>
                 );
             case 'membership':
@@ -69,16 +103,18 @@ const Welcome = () => {
                         <p class="text24">Custom appearance</p>
                         <p class="text24">Longer posts</p>
                         <p class="text24">Voting</p>
-                        <button className="button join">Buy membership</button>
+                        <button className="light-button join">Buy membership</button>
                     </div>
                 );
             case 'privacy':
                 return (
                     <div className="channel-content">
                         <p class="text36">Privacy</p>
-                        <p class="text24">Totally encrypted</p>
+                        <p class="text24">All data is encrypted, never leaves Aether</p>
                         <p class="text24">Your data is for you, not us</p>
-                        <button className="button join">Join for free</button>
+                        <Link to="/join">
+                            <button className="light-button join">Join</button>
+                        </Link>
                     </div>
                 );
             default:
@@ -92,6 +128,7 @@ const Welcome = () => {
     return (
         <div className="container">
             <aside id="left-aside">
+                <p id="aside-welcome-text" onClick={() => setCurrentView('welcome')}>Aether</p>
                 <div id="welcome-items">
                     <nav>
                         <ul>
@@ -108,17 +145,30 @@ const Welcome = () => {
             </aside>
             <main>
                 <header id="base-header">
-                    <p class="text36" onClick={() => setCurrentView('welcome')}>Welcome</p>
+                    <div className="spacer"></div>
+                        <form id="search-form" onSubmit={(e) => {
+                            e.preventDefault();
+                            handleSearchClick();
+                        }}>
+                            <div className="search-container">
+                                <button className="icon-button ask" data-tooltip="Ask" type="button" onClick={handleAskClick}>
+                                    <img className="standard-icon" src="/media/site_images/icons/ask.png" alt="Ask"/>
+                                </button>
+                                <input id="search-bar" type="text" name="keyword" placeholder="Type..." value={currentQuery} onChange={(e) => setCurrentQuery(e.target.value)}/>
+                                <button className="icon-button search" data-tooltip="Search" type="submit" onClick={handleSearchClick}>
+                                    <img className="standard-icon" src="/media/site_images/icons/search.png" alt="Search"/>
+                                </button>
+                            </div>
+                        </form>
+                <div className="spacer"></div>
+                <button id="messages-button">Messages</button>
                 </header>
                 <div className="content">
                     <div className="home-container">
                         <div className="content-feed">
                             {renderComponent()} 
                         </div>
-                        <div id="right-aside">
-                            <Link to={'/login'}>
-                                <button className="button join">Login</button>
-                            </Link>
+                        <div id="right-aside" class="light">
                         </div>
                     </div>
                 </div>
