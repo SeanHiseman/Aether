b51984dcf25b02677757db58bd934b6aa980c704
Sean Hiseman <seanhiseman@gmail.com>
2024-08-26 22:50:56 +0100

    Extra themes only available to members

--- frontend/src/pages/profiles/settings/membership.js
+++ frontend/src/pages/profiles/settings/membership.js
@@ -13,19 +13,26 @@ const MembershipSettings = ({ user }) => {
                     <p class="text24">Your support helps us grow</p>
                     <p class="text24">If you wish to cancel, do it here</p>
                     <button className="button delete">Cancel membership</button>
-                    {errorMessage && <div className="error-message">{errorMessage}</div>}
+                    <div className="error-message">{errorMessage}</div>
                 </div>
             ) : (
                 <div id="display-area">
-                    <p class="text36">Membership benefits</p>
-                    <p class="text24">Earn money from posts</p>
-                    <p class="text24">Copilot</p>
-                    <p class="text24">Adjustable feeds</p>
-                    <p class="text24">Custom appearance</p>
-                    <p class="text24">Longer posts</p>
-                    <p class="text24">Voting</p>
+                    <div className="left-aligned-text">
+                        <p class="text36">Membership</p>
+                        <div className="spacer20px"/>
+                        <p class="text36">Ask</p>
+                        <p class="text24">-Ask is an assistant that aids throughout Aether</p>
+                        <p class="text24">-Need to check the accuracy of a post, find the perfect content or get help using the site? Just Ask</p>
+                        <div className="spacer20px"/>
+                        <p class="text36">Benefits</p>
+                        <p class="text24">-Earn money from posts. The more upvotes and fewer downvotes, the more you make per view</p>
+                        <p class="text24">-Custom personal feeds. Full control over how you sort your content</p>
+                        <p class="text24">-Dive in to detail by writing longer posts. Up to 100,000 characters</p>
+                        <p class="text24">-Voting for moderators, admins, leaders and features</p>
+                        <p class="text24"></p>
+                    </div>
                     <button className="button join">Join</button>
-                    {errorMessage && <div className="error-message">{errorMessage}</div>}
+                    <div className="error-message">{errorMessage}</div>
                 </div>
             )}
         </div>
