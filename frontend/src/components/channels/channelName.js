a5fa37140aee6f525dc1400c8e4046b309404344
Sean Hiseman <seanhiseman@gmail.com>
2024-08-29 20:46:43 +0100

    Fixed new ask chat creation

--- frontend/src/components/channels/channelName.js
+++ frontend/src/components/channels/channelName.js
@@ -74,7 +74,7 @@ function ChannelName({ channelId, channelName, channelType, locationName, channe
                             <button className="button" onClick={() => {
                                 setIsEditingChannelName(true);
                                 setNewChannelName(channelName);
-                            }}>Edit name</button>
+                            }}>Rename</button>
                         </div>
                     )}
                 </div>
