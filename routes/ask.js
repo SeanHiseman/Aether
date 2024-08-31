6920b164835e9cdd9c3829fdb41ae7553c0e227b
Sean Hiseman <seanhiseman@gmail.com>
2024-08-31 16:57:26 +0100

    Implemented Ask functionality to posts

--- routes/ask.js
+++ routes/ask.js
@@ -3,11 +3,65 @@ import dotenv from 'dotenv';
 import OpenAI from "openai";
 import { Router } from 'express';
 import { v4 } from 'uuid';
-import { AskChats, AskMessages } from '../models/models.js';
+import { AskChats, AskMessages, AskNotes } from '../models/models.js';
 
 dotenv.config();
 const openai = new OpenAI();
 const router = Router();
+
+router.post('/ask_button', authenticateCheck, async (req, res) => {
+    try {
+        const { postTitle, postContent, postId } = req.body;
+        const combinedContent = `Title: ${postTitle}, Content: ${postContent}`;
+
+        //Creates API assistant
+        const assistant = await openai.beta.assistants.create({
+            name: "Ask",
+            instructions: "Correct misinformation",
+            model: "gpt-4o-mini",
+        });
+
+        //Send user message to OpenAI
+        const thread = await openai.beta.threads.create();
+        const post = await openai.beta.threads.messages.create(
+            thread.id,
+            {
+                role: "user",
+                content: combinedContent
+            }
+        );
+
+        //Run OpenAI assistant
+        const run = await openai.beta.threads.runs.create(
+            thread.id,
+            {
+                assistant_id: assistant.id, 
+                instructions: "Check input for misinformation. State if none present. Definite tone. Reply length < 3 sentences if possible"
+            }
+        );
+
+        //Wait for OpenAI response
+        let runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+        while (runStatus.status !== "completed") {
+            runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+        }
+
+        //Get OpenAI response
+        const messages = await openai.beta.threads.messages.list(thread.id);
+        const aiReply = messages.data.find(msg => msg.role === 'assistant').content[0].text.value;
+
+        //Save user message 
+        const newNote = await AskNotes.create({
+            note_id: v4(),
+            post_id: postId,
+            note_content: aiReply,
+            timestamp: Date.now()
+        });
+        res.status(200).json({ newNote });
+    } catch (error) {
+        res.status(500).json({ error: "Ask button error" });
+    }
+});
 
 router.post('/change_ask_chat_name', authenticateCheck, async (req, res) => {
     try {
