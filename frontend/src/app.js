2d253854f7ed231dde0b2f4f0930deb60044be88
Sean Hiseman <seanhiseman@gmail.com>
2024-09-23 19:39:01 +0100

    Reverted useQuery in groupHome

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -44,9 +44,9 @@ const App = () => {
                         <Route path="/login" element={<Login />} />
                         <Route path="/join" element={<Join />} />
                         <Route path="/" element={<QueryProvider><AuthCheck><BaseLayout /></AuthCheck></QueryProvider>}>
-                                <Route path="ask" element={<AuthCheck><AskChannel /></AuthCheck>} >
-                                    <Route path=":chatId" element={<QueryProvider><AuthCheck><AskChannel /></AuthCheck></QueryProvider>} />
-                                </Route>
+                            <Route path="ask" element={<AuthCheck><AskChannel /></AuthCheck>} >
+                                <Route path=":chatId" element={<QueryProvider><AuthCheck><AskChannel /></AuthCheck></QueryProvider>} />
+                            </Route>
                             <Route path="p/:feed_name" element={<AuthCheck><PersonalFeed/></AuthCheck>} />
                             <Route path="settings/:username" element={<AuthCheck><Settings /></AuthCheck>} />
                             <Route path="group_settings/:group_name" element={<AuthCheck><GroupSettings /></AuthCheck>} />
