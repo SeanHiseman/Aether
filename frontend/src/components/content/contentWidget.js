8c1d14ef6a6cdbf43c64d4aa07c086302f713227
Sean Hiseman <63054318+SeanHiseman@users.noreply.github.com>
2025-03-01 13:40:04 +0000

    Build update

--- frontend/src/components/content/contentWidget.js
+++ frontend/src/components/content/contentWidget.js
@@ -193,9 +193,7 @@ const ContentWidget = ({ canRemove, feed, isGroup, onEditClick, onPostRemoved, o
             <button className={`large-icon ${upvoteClass}`} disabled={downvoteLimit || upvoteLimit} onClick={() => postVote(post.post_id, 'upvote')}>
               <FaArrowUp />
             </button>}
-            <span className="total-votes">
-              {upvotes - downvotes} {Math.abs(upvotes - downvotes) === 1 ? 'vote' : 'votes'}{isViewingOwnPost && ' (your post)'}
-            </span>
+            <span className="total-votes">{upvotes - downvotes} {Math.abs(upvotes - downvotes) === 1 ? 'vote' : 'votes'}</span>
           {!isViewingOwnPost &&           
             <button className={`large-icon ${downvoteClass}`} disabled={downvoteLimit || upvoteLimit} onClick={() => postVote(post.post_id, 'downvote')} >
               <FaArrowDown />
