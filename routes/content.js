ee831ab035bc30b4d60005e33a97e61100d1f7b5
Sean Hiseman <63054318+SeanHiseman@users.noreply.github.com>
2025-03-19 14:30:54 +0000

    Layout adjustment

--- routes/content.js
+++ routes/content.js
@@ -22,7 +22,6 @@ const calculateFileSizes = (files) => {
 const checkStorageLimit = async (req, res, next) => {
     try {
         const user = await Users.findByPk(req.session.user_id);
-        console.log("user:", user);
         if (!user) {
             return res.status(404).json({ success: false, message: 'User not found' });
         }
@@ -36,7 +35,6 @@ const checkStorageLimit = async (req, res, next) => {
         req.currentUser = user;
         next();
     } catch (error) {
-        console.log('Error checking storage limit:', error);
         return res.status(500).json({ success: false, error: error.message });
     }
 };
@@ -216,7 +214,6 @@ const post_upload = multer({
 router.post('/create_post', authenticateCheck, checkStorageLimit, post_upload.array('files'), async (req, res) => {
     try {
         let { channel_id, content, feed_id, parent_id, post_id, poster_id, title } = req.body;
-        console.log("req.body:", req.body);
         if (!post_id) {
             post_id = v4();
         }
@@ -264,7 +261,6 @@ router.post('/create_post', authenticateCheck, checkStorageLimit, post_upload.ar
             poster_id,
             title
         });
-        console.log("post:", post);
         if (parent_id) { //parent_id means post is a reply
             const parentPost = await Posts.findOne({ where: { post_id: parent_id } });
             if (parentPost) {
@@ -274,7 +270,6 @@ router.post('/create_post', authenticateCheck, checkStorageLimit, post_upload.ar
         }
         return res.status(200).json({ success: true, post });
     } catch (error) {
-        console.error('Error creating post:', error);
         if (req.files && req.files.length > 0) {
             req.files.forEach(file => {
                 try {
