0443cd8a8b4ead25c0083b28318089ab5abdd765
Sean Hiseman <63054318+SeanHiseman@users.noreply.github.com>
2025-03-19 11:15:33 +0000

    fix

--- routes/content.js
+++ routes/content.js
@@ -21,7 +21,7 @@ const calculateFileSizes = (files) => {
 
 const checkStorageLimit = async (req, res, next) => {
     try {
-        const user = await Users.findByPk(req.session.user.user_id);
+        const user = await Users.findByPk(req.session.user_id);
         console.log("user:", user);
         if (!user) {
             return res.status(404).json({ success: false, message: 'User not found' });
