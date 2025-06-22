f95b8a904df09bfb918d8fdb19b98fb0e8c3109f
Sean Hiseman <63054318+SeanHiseman@users.noreply.github.com>
2025-06-22 15:34:56 +0100

    Removed duplicate views

--- frontend/src/components/content/contentWidget.js
+++ frontend/src/components/content/contentWidget.js
@@ -302,7 +302,6 @@ const ContentWidget = ({ canRemove, feed, isDraft, isGroup, onEditClick, onPostR
 						)}
 					</>
 				)}
-				{!isDraft && (<p className="text16">{views} {views === 1 ? 'view' : 'views'}</p>)}
 				{isAuthenticated && post.poster_id === viewer.feed_id && !readOnly && (
 					<button className="large-icon" onClick={() => onEditClick(post)} title={isReply ? "Edit Reply" : isDraft ? "Edit draft" : "Edit Post"}>
 						<FaEdit />
