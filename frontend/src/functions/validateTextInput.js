dbc2f526ca274dccbc278dca23300639ae6bcbcb
Sean Hiseman <63054318+SeanHiseman@users.noreply.github.com>
2026-01-02 17:18:30 +0000

    Fixed character check

--- frontend/src/functions/validateTextInput.js
+++ frontend/src/functions/validateTextInput.js
@@ -10,7 +10,7 @@ function ValidateTextInput(textInput, minLength = 0, maxLength = 9999999, charac
 		return { valid: false, error: `No more than ${maxLength} characters.` };
 	}
 	if (characterCheck && normalisedInput.length > 0) {
-		const characterRegex = /^[\p{L}\p{N}_-]+$/u;
+		const characterRegex = /^[\p{L}\p{N}_\s-]+$/u;
 		if (!characterRegex.test(normalisedInput)) {
 			return { valid: false, error: 'Contains invalid characters.' };
 		}
